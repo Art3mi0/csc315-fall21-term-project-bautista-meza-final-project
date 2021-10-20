88d4b67f70be0b7c8c61b935b334972494e4f2e4
Artemio <temomez123@gmail.com>
2021-10-19 21:46:49 -0400

    Got all login, signup, and logout code done

--- app/src/main/java/com/hastellc/workoutapp/MainActivity.java
+++ app/src/main/java/com/hastellc/workoutapp/MainActivity.java
@@ -1,14 +1,152 @@
 package com.hastellc.workoutapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private static final String TAG = "MainActivity";
+
+    private ConstraintLayout mLoggedInGroup;
+    private ConstraintLayout mLoggedOutGroup;
+    private TextView mNameLabel;
+    private EditText mEmailField;
+    private EditText mPasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLoggedInGroup = findViewById(R.id.logged_in_group);
+        mLoggedOutGroup = findViewById(R.id.logged_out_group);
+        mNameLabel = findViewById(R.id.hello);
+        mEmailField = findViewById(R.id.email);
+        mPasswordField = findViewById(R.id.password);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+
+    }
+
+    public void signOut(View view) {
+        mAuth.signOut();
+        updateUI(null);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if (currentUser != null) {
+            mLoggedOutGroup.setVisibility(View.GONE);
+            mLoggedInGroup.setVisibility(View.VISIBLE);
+            mNameLabel.setText(String.format(getResources().getString(R.string.hello), currentUser.getEmail()));
+        } else {
+            mLoggedInGroup.setVisibility(View.GONE);
+            mLoggedOutGroup.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //reload();
+        }
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Required.");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Required.");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+
+        return valid;
+    }
+
+    public void signIn(View view) {
+        if (!validateForm()) {
+            return;
+        }
+
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Exception e = task.getException();
+                            Log.w(TAG, "signInWithEmail:failure", e);
+                            Toast.makeText(MainActivity.this, "Login failed: " + e.getLocalizedMessage(),
+                                    Toast.LENGTH_LONG).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    public void createAccount() {
+        if (!validateForm()) {
+            return;
+        }
+
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
     }
 }
